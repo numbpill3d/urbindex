efe4d2ced1f64276823b5b26a08ad0c7d8d6619d
numbpill3d <nolove@incel.email>
2025-03-11 12:29:20 -0400

    fb

--- js/config.js
+++ js/config.js
@@ -2,13 +2,13 @@
 // Replace with your own Firebase project configuration
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA1234567890abcdefghijklmnopqrstuv",
-  authDomain: "urbindex-demo.firebaseapp.com",
-  projectId: "urbindex-demo",
-  storageBucket: "urbindex-demo.appspot.com",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:abcdef1234567890",
-  measurementId: "G-ABCDEF1234"
+  apiKey: "AIzaSyCifqLPnCuvRVZ9hvHIJVBmHkzB5nJyqtc",
+  authDomain: "urbindex-d69e1.firebaseapp.com",
+  projectId: "urbindex-d69e1",
+  storageBucket: "urbindex-d69e1.firebasestorage.app",
+  messagingSenderId: "889914696327",
+  appId: "1:889914696327:web:351daa656a4d12fa828e22",
+  measurementId: "G-HFS8F0KZSH"
 };
 
 // NOTE: The above configuration is a placeholder.
