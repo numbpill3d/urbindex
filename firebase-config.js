c829453c4d2f845321fb8c218cf7bf27d9940b02
numbpilled <numbpilled@gmail.com>
2025-10-24 19:58:33 +0000

    yes

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCifqLPnCuvRVZ9hvHIJVBmHkzB5nJyqtc",
+    authDomain: "urbindex-d69e1.firebaseapp.com",
+    projectId: "urbindex-d69e1",
+    storageBucket: "urbindex-d69e1.firebasestorage.app",
+    messagingSenderId: "889914696327",
+    appId: "1:889914696327:web:351daa656a4d12fa828e22"
+};
